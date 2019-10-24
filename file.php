2d5edd9e1e5b497229abbcca883caa230b2e73c0
CaptN3M070 <56826081+CaptN3M070@users.noreply.github.com>
2019-10-24 12:15:19 +0530

    initial commit

---
+++ file.php
@@ -0,0 +1,21 @@
+<?php
+//basic file upload 
+
+$dir = "uploads/"; //directory to upload
+$file = $dir . basename($_FILES["fileToUpload"]["file_name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+
+if(isset($_POST["submit"])) { //submitting button 
+
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+?>
